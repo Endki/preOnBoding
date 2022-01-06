15bc5033fe934518d84e76849541e14c31d5e834
KSJ <khb3221@naver.com>
2022-01-06 22:51:15 +0900

    #feat 기초 navbar

--- preonboarding/src/index.js
+++ preonboarding/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Main from './Main';
+import Navbar from './components/Navbar/Navbar';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Navbar/>
+    <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
